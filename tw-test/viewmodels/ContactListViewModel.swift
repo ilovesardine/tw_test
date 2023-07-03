29c250721be6d921b45d90ca8fe13c87a8eb172b
Krisnandika <krisnandikaaji@gmail.com>
2023-07-03 13:43:09 +0700

    contact list: implement tests & vm

--- tw-test/viewmodels/ContactListViewModel.swift
+++ tw-test/viewmodels/ContactListViewModel.swift
@@ -17,12 +17,60 @@ class ContactListViewModel {
     init() {
         data = []
         filteredData = []
+        
+        commonInit()
     }
     
-    func fetchLocalJsonFile() {}
-    func searchContacts(keyword: String) {}
-    func addContact(_ contact: Contact) {}
-    func updateContact(_ contact: Contact) {}
+    private func commonInit() {
+        fetchLocalJsonFile()
+    }
     
-    func selectContact(_ contact: Contact) {}
+    func refreshData() {
+        fetchLocalJsonFile()
+    }
+    
+    func fetchLocalJsonFile() {
+        let bundle = Bundle(for: type(of: self))
+
+        guard let url = bundle.url(forResource: "data", withExtension: "json") else {
+            print("Missing json file")
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            
+            let contacts = try decoder.decode([Contact].self, from: data)
+            
+            self.data = contacts
+            self.filteredData = contacts
+        } catch {
+            print("Exception in parsing data")
+        }
+    }
+    
+    func searchContacts(keyword: String) {
+        self.keyword = keyword
+        
+        let filtered = data.filter({ $0.firstName!.lowercased().contains(keyword.lowercased()) || $0.lastName!.lowercased().contains(keyword.lowercased()) })
+        
+        self.filteredData = filtered
+    }
+    
+    func addContact(_ contact: Contact) {
+        self.data.append(contact)
+        self.filteredData = data
+    }
+    
+    func updateContact(_ contact: Contact) {
+        guard let index = self.data.firstIndex(where: { $0.id == contact.id }) else { return }
+        
+        self.data[index] = contact
+        self.filteredData = data
+    }
+    
+    func selectContact(_ contact: Contact) {
+        self.selectionHandler?(contact)
+    }
 }
